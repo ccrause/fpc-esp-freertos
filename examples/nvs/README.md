614aad0131c7ff3eac78e94e41e3d732be258000
ccrause <christo.crause@gmail.com>
2020-05-09 15:02:34 +0200

    Nonvolatile storage example.

---
+++ examples/nvs/README.md
@@ -0,0 +1,7 @@
+# Simple non-volatile storage (NVS) example
+This example demonstrates the following:
+* Create a handle to access NVS.
+* Read and print value of a restart counter stored in NVS.
+* Increment value and store to NVS.
+
+When next the controller boots up, it should show an incremented counter.
---
+++ examples/nvs/nvs_test.pp
@@ -0,0 +1,49 @@
+program nvs_test;
+
+uses
+  nvs, esp_err;
+
+var
+  err: longint;
+  my_handle: Tnvs_handle_t;
+  restart_counter: int32 = 0; // value will default to 0, if not set yet in NVS
+
+begin
+  err := nvs_flash_init();
+  if ((err = ESP_ERR_NVS_NO_FREE_PAGES) or (err = ESP_ERR_NVS_NEW_VERSION_FOUND)) then
+  begin
+    EspErrorCheck(nvs_flash_erase());
+    err := nvs_flash_init();
+  end;
+  EspErrorCheck(err);
+
+  writeln('Opening Non-Volatile Storage (NVS) handle... ');
+  err := nvs_open('storage', NVS_READWRITE, @my_handle);
+  if not(err = ESP_OK) then
+    writeln('Error opening NVS handle: ', err)
+  else
+    writeln('Done');
+
+  writeln('Reading restart counter from NVS ... ');
+  err := nvs_get_i32(my_handle, 'restart_counter', @restart_counter);
+  case err of
+    ESP_OK:
+      writeln('Restart counter = ', restart_counter);
+    ESP_ERR_NVS_NOT_FOUND:
+      writeln('The value is not initialized yet!');
+    else
+      writeln('Error reading NVS: ', err);
+  end;
+
+  inc(restart_counter);
+  err := nvs_set_i32(my_handle, 'restart_counter', restart_counter);
+  if not(err = ESP_OK) then
+    writeln('Error writing restart counter: ', esp_err_to_name(err))
+  else
+  begin
+    err := nvs_commit(my_handle);
+    if not(err = ESP_OK) then
+      writeln('Error writing restart counter: ', esp_err_to_name(err));
+    nvs_close(my_handle);
+  end;
+end.
