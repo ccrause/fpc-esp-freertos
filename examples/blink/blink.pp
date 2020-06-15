017708cdaae0db3c63fd39ac97e0d415001227ba
ccrause <christo.crause@gmail.com>
2020-06-15 12:30:53 +0200

    Blink example now works for both ESP32 and ESP8266

--- examples/blink/blink.pp
+++ examples/blink/blink.pp
@@ -1,18 +1,36 @@
 program blink;
 
 uses
-  freertos, gpio, gpio_types;
+  freertos, gpio, task, portmacro
+  {$ifdef CPULX6}
+  , gpio_types
+  {$endif}
+  ;
 
 const
-  LED = GPIO_NUM_2;
+  LED = GPIO_NUM_2;  // NodeMCU LED on ESP-12E module, also ESP32 DevKit V1 from DOIT
+  //LED = GPIO_NUM_13; // Sonoff Basic
 
 procedure sleep(Milliseconds: cardinal);  // Should be in SysUtils but including it causes an error in ESP32
 begin
   vTaskDelay(Milliseconds div portTICK_PERIOD_MS);
 end;
 
+var
+  cfg: Tgpio_config;
+
 begin
-  gpio_pad_select_gpio(ord(LED));
+//  Below function only implemented on ESP32, on ESP8266 it is implemented as a convoluted macro
+//  gpio_pad_select_gpio(ord(LED));
+
+// For compatibility rather set the GPIO function using the gpio_config function
+  cfg.pin_bit_mask := 1 shl ord(LED);
+  cfg.mode := GPIO_MODE_OUTPUT;
+  cfg.pull_up_en := GPIO_PULLUP_DISABLE;
+  cfg.pull_down_en := GPIO_PULLDOWN_DISABLE;
+  cfg.intr_type := GPIO_INTR_DISABLE;
+  gpio_config(cfg);
+
   gpio_set_direction(LED, GPIO_MODE_OUTPUT);
   repeat
     writeln('.');
