f230071df0e9c79687390367e4a54f02cfab7761
ccrause <christo.crause@gmail.com>
2020-06-20 10:52:36 +0200

    Removed redundant cdecl

--- freertos-fpc/esp8266-idf/esp_event_loop.pp
+++ freertos-fpc/esp8266-idf/esp_event_loop.pp
@@ -11,13 +11,13 @@ const
   EVENT_LOOP_STACKSIZE = CONFIG_EVENT_LOOP_STACK_SIZE;
 
 type
-  Tsystem_event_cb = function(ctx: pointer; event: Psystem_event): Tesp_err; cdecl;
+  Tsystem_event_cb = function(ctx: pointer; event: Psystem_event): Tesp_err;
 
 function esp_event_loop_init(cb: Tsystem_event_cb; ctx: pointer): Tesp_err;
-  cdecl; external;
+  external;
 function esp_event_loop_set_cb(cb: Tsystem_event_cb;
-  ctx: pointer): Tsystem_event_cb; cdecl; external;
-function esp_event_loop_get_queue: TQueueHandle; cdecl; external;
+  ctx: pointer): Tsystem_event_cb; external;
+function esp_event_loop_get_queue: TQueueHandle; external;
 
 implementation
 
