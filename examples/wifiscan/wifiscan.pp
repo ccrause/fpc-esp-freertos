8d17ddbf5bd401c533bd718b19ca6be99391fd45
ccrause <christo.crause@gmail.com>
2020-08-30 13:47:50 +0200

    Merged ESP32 and ESP8266 code.  Added checks for invalid enumeration values in printAPInfo.

--- examples/wifiscan/wifiscan.pp
+++ examples/wifiscan/wifiscan.pp
@@ -3,8 +3,18 @@ program wifiscan;
 {$include sdkconfig.inc}
 
 uses
-  esp_err, esp_wifi, esp_wifi_types, esp_netif, esp_event,
-  esp_wifi_default, nvs;
+  esp_err, esp_wifi, esp_wifi_types, esp_netif, esp_event
+  {$ifdef CPULX6}, esp_wifi_default{$endif}
+  , nvs
+  {$ifdef CPULX106}, nvs_flash, esp_event_loop{$endif};
+
+{$ifdef CPULX106}
+function eventhandler(ctx: pointer; event: Psystem_event): Tesp_err;
+begin
+  writeln('Event -  ', event^.event_id);
+  Result := ESP_OK;
+end;
+{$endif}
 
 procedure printAPInfo(const AP: Twifi_ap_record);
 var
@@ -31,12 +41,27 @@ begin
   writeln;
 
   writeln('  primary: ', AP.primary);
-  writeln('  secondary: ', AP.second);
+  if ord(AP.second) <= ord(WIFI_SECOND_CHAN_BELOW) then
+    writeln('  secondary: ', AP.second)
+  else
+    writeln('  secondary: INVALID');
   writeln('  rssi: ', AP.rssi);
-  writeln('  AuthMode: ', AP.authmode);
-  writeln('  Pairwise cipher: ', AP.pairwise_cipher);
-  writeln('  Group cipher: ', AP.group_cipher);
-  writeln('  Antenna: ', AP.ant);
+  if ord(AP.authmode) < ord(WIFI_AUTH_MAX) then
+    writeln('  AuthMode: ', AP.authmode)
+  else
+    writeln('  AuthMode: INVALID');
+  if ord(AP.pairwise_cipher) <= ord(WIFI_CIPHER_TYPE_UNKNOWN) then
+    writeln('  Pairwise cipher: ', AP.pairwise_cipher)
+  else
+    writeln('  Pairwise cipher: INVALID');
+  if ord(AP.group_cipher) <= ord(WIFI_CIPHER_TYPE_UNKNOWN) then
+    writeln('  Group cipher: ', AP.group_cipher)
+  else
+    writeln('  Group cipher: INVALID');
+  if ord(AP.ant) <= ord(WIFI_ANT_MAX) then
+    writeln('  Antenna: ', AP.ant)
+  else
+    writeln('  Antenna: INVALID');
   writeln('  Supported wifi modes');
   writeln('    b: ', boolean(AP.phy_11b));
   writeln('    g: ', boolean(AP.phy_11g));
@@ -60,24 +85,31 @@ begin
     writeln('    Start channel: ', schan);
     writeln('    End channel: ', nchan);
     writeln('    Max power: ', max_tx_power);
-    writeln('    Policy: ', policy);
+    if ord(policy) <= ord(WIFI_COUNTRY_POLICY_MANUAL) then
+      writeln('    Policy: ', policy)
+    else
+      writeln('    Policy: INVALID');
   end;
 end;
 
 procedure wifi_scan;
 const
-  number = 1;
+  number = 10;
 var
-  sta_netif: Pesp_netif;
+  {$ifdef CPULX6}sta_netif: Pesp_netif;{$endif}
   cfg: Twifi_init_config;
   ap_info: array[0..number-1] of Twifi_ap_record;
   ap_info_len: uint16 = number;
   i: integer;
 begin
   writeln('Enter scan');
+  {$ifdef CPULX6}
   EspErrorCheck(esp_netif_init());
   EspErrorCheck(esp_event_loop_create_default());
   sta_netif := esp_netif_create_default_wifi_sta();
+  {$else}
+  EspErrorCheck(esp_event_loop_init(@eventhandler, nil));
+  {$endif}
   WIFI_INIT_CONFIG_DEFAULT(cfg);
   EspErrorCheck(esp_wifi_init(@cfg));
   EspErrorCheck(esp_wifi_set_mode(WIFI_MODE_STA));
@@ -85,6 +117,7 @@ begin
   EspErrorCheck(esp_wifi_scan_start(nil, true));
   ap_info_len := length(ap_info);
   EspErrorCheck(esp_wifi_scan_get_ap_records(@ap_info_len, @ap_info[0]));
+  EspErrorCheck(esp_wifi_scan_get_ap_num(@ap_info_len));
 
   for i := 0 to ap_info_len-1 do
   begin
@@ -99,7 +132,7 @@ var
 
 begin
   ret := nvs_flash_init();
-  if (ret = ESP_ERR_NVS_NO_FREE_PAGES) or (ret = ESP_ERR_NVS_NEW_VERSION_FOUND) then
+  if (ret = ESP_ERR_NVS_NO_FREE_PAGES) {$ifdef CPULX6}or (ret = ESP_ERR_NVS_NEW_VERSION_FOUND){$endif} then
   begin
     nvs_flash_erase();
     ret := nvs_flash_init();
