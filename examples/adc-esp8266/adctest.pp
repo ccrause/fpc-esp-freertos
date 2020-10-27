9b625c4f6ab62d3b357e3866c0e85b1e3031c8d1
ccrause <christo.crause@gmail.com>
2020-10-27 19:06:47 +0200

    Fixed up averaging calculation. Slowed down ADC clock for better accuracy.

--- examples/adc-esp8266/adctest.pp
+++ examples/adc-esp8266/adctest.pp
@@ -6,30 +6,30 @@ uses
   esp_err, adc, task, portmacro;
 
 const
-  NO_OF_SAMPLES = 64;
+  NO_OF_SAMPLES = 64;  // Note: more samples could lead to 16 bit overflow (65 * 1023 = 66496)
 
 var
   adc_config: Tadc_config;
-  adc_reading, adc_total: uint16;
+  adc_reading, adc_average: uint16;
   i: integer;
 
 begin
   // Depend on menuconfig->Component config->PHY->vdd33_const value
   // When measuring system voltage(ADC_READ_VDD_MODE), vdd33_const must be set to 255.
   adc_config.mode := ADC_READ_TOUT_MODE;
-  adc_config.clk_div := 8; // ADC sample collection clock = 80MHz/clk_div = 10MHz
+  adc_config.clk_div := 32; // ADC sample collection clock = 80MHz/clk_div = 2.5MHz
   EspErrorCheck(adc_init(@adc_config));
 
   repeat
-    adc_total := 0;
+    adc_average := 0;
     for i := 0 to NO_OF_SAMPLES-1 do
     begin
       EspErrorCheck(adc_read(@adc_reading));
-      adc_reading := adc_reading + adc_reading;
+      adc_average := adc_average + adc_reading;
     end;
 
-    adc_total := adc_total div NO_OF_SAMPLES;
-    writeln('ADC', ' Raw: ', adc_reading);
+    adc_average := adc_average div NO_OF_SAMPLES;
+    writeln('ADC: ', adc_average);
 
     vTaskDelay(1000 div portTICK_PERIOD_MS);
   until false;
