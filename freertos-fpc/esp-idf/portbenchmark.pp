fda6dec96253da5ef29b632e4b01d3697a4681e5
ccrause <christo.crause@gmail.com>
2021-04-21 06:56:48 +0200

    Add missing unit portbenchmark

---
+++ freertos-fpc/esp-idf/portbenchmark.pp
@@ -0,0 +1,44 @@
+unit portbenchmark;
+
+interface
+
+procedure portbenchmarkINTERRUPT_DISABLE(); inline;
+procedure portbenchmarkINTERRUPT_RESTORE(newstate: uint32); inline;
+procedure portbenchmarkIntLatency(); inline;
+procedure portbenchmarkIntWait(); inline;
+procedure portbenchmarkReset(); inline;
+procedure portbenchmarkPrint(); inline;
+
+implementation
+
+procedure portbenchmarkINTERRUPT_DISABLE();
+begin
+
+end;
+
+procedure portbenchmarkINTERRUPT_RESTORE(newstate: uint32);
+begin
+
+end;
+
+procedure portbenchmarkIntLatency();
+begin
+
+end;
+
+procedure portbenchmarkIntWait();
+begin
+
+end;
+
+procedure portbenchmarkReset();
+begin
+
+end;
+
+procedure portbenchmarkPrint();
+begin
+
+end;
+
+end.
