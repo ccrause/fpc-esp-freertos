11d487000ddef1eccae81ab1dbf9ac81e104ae92
ccrause <christo.crause@gmail.com>
2025-02-24 07:46:24 +0200

    Remove _t from type name.

--- freertos-fpc/esp8266-idf/esp_log.pp
+++ freertos-fpc/esp8266-idf/esp_log.pp
@@ -3,14 +3,14 @@ unit esp_log;
 interface
 
 type
-  Pesp_log_level_t = ^Tesp_log_level_t;
-  Tesp_log_level_t = (ESP_LOG_NONE, ESP_LOG_ERROR, ESP_LOG_WARN,
+  Pesp_log_level = ^Tesp_log_level;
+  Tesp_log_level = (ESP_LOG_NONE, ESP_LOG_ERROR, ESP_LOG_WARN,
     ESP_LOG_INFO, ESP_LOG_DEBUG, ESP_LOG_VERBOSE, ESP_LOG_MAX);
 
-  Tputchar_like_t = function(ch: int32): int32; cdecl;
+  Tputchar_like = function(ch: int32): int32; cdecl;
 
-procedure esp_log_level_set(tag: PChar; level: Tesp_log_level_t); external;
-function esp_log_set_putchar(func: Tputchar_like_t): Tputchar_like_t; external;
+procedure esp_log_level_set(tag: PChar; level: Tesp_log_level); external;
+function esp_log_set_putchar(func: Tputchar_like): Tputchar_like; external;
 
 function esp_log_timestamp: uint32; external;
 function esp_log_early_timestamp: uint32; external;
@@ -19,8 +19,8 @@ function esp_log_early_timestamp: uint32; external;
 // to typecheck formatting against printf style.
 // This check will obviously not be done in FPC
 // Perhaps preformat string and pass final string as format parameter below
-procedure esp_log_write(level: Tesp_log_level_t; tag: PChar; format: PChar); varargs; external;
-procedure esp_early_log_write(level: Tesp_log_level_t; tag: PChar; format: PChar); varargs; external;
+procedure esp_log_write(level: Tesp_log_level; tag: PChar; format: PChar); cdecl; varargs; external;
+procedure esp_early_log_write(level: Tesp_log_level; tag: PChar; format: PChar); cdecl; varargs; external;
 
 implementation
 
